58de38391346ebb09fae3801eef95eb2e96cdbec
BBergsJ <goldlike888@gmail.com>
2022-01-14 10:39:46 +0300

    Spring DataSource [#6878]

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE types (
+    id serial primary key,
+    name varchar(100)
+);
+
+CREATE TABLE rules (
+    id serial primary key,
+    name varchar(100)
+);
+
+CREATE TABLE accident (
+    id serial primary key,
+    name varchar(100),
+    text varchar(2000),
+    address varchar(100),
+    type_id int references types
+);
+
+CREATE TABLE accident_rules (
+    acc_id int references accident,
+    rule_id int references rules
+);
+
+INSERT INTO rules (name) VALUES ('Статья. 1'), ('Статья. 2'), ('Статья. 3');
+INSERT INTO types (name) VALUES ('Две машины'), ('Машина и человек'), ('Машина и велосипед');
--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,20 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accident.repository.AccidentJdbcTemplate;
 import ru.job4j.accident.repository.AccidentMem;
 import ru.job4j.accident.service.AccidentService;
 
 @Controller
 public class IndexControl {
-    private AccidentService accidentService;
+    private final AccidentJdbcTemplate accidents;
 
-    public IndexControl(AccidentService accidentService) {
-        this.accidentService = accidentService;
+    public IndexControl(AccidentJdbcTemplate accidents) {
+        this.accidents = accidents;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", accidentService.findAll());
+        model.addAttribute("accidents", accidents.getAll());
         model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
