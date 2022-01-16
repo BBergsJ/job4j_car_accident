9a09c32248459bd10fddf15d9ae3745c2883b3a2
BBergsJ <goldlike888@gmail.com>
2022-01-16 16:28:09 +0300

    @Transactional added 2.0

--- src/main/java/ru/job4j/accident/config/JdbcConfig.java
+++ src/main/java/ru/job4j/accident/config/JdbcConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
@@ -32,4 +35,10 @@ public class JdbcConfig {
         return new JdbcTemplate(ds);
     }
 
+    @Bean
+    public PlatformTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
 }
